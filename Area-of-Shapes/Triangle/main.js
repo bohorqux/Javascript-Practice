dfa155b4456886b7caee63dcf94b6fc20e6e1b27
Xavier Bohorquez <bohorqux@bu.edu>
2018-11-18 17:18:56 -0500

    Configured code to be object oriented

--- Area-of-Shapes/Triangle/main.js
+++ Area-of-Shapes/Triangle/main.js
@@ -1,43 +1,103 @@
 let clicks = 1;
 
-let Positions = {
-    coordinate1: undefined,
-    coordinate2: undefined,
-    coordinate3: undefined,
+function Point(x, y) {
+    this.position = function(n) {
+        let canvas = document.getElementById('triangle');
+        let rect = canvas.getBoundingClientRect();
+        return n == x ? x - rect.left : y - rect.top;
+    };
+    this.window_x = x;
+    this.window_y = y;
+    this.canvas_x = Math.trunc(this.position(x));
+    this.canvas_y = Math.trunc(this.position(y));
 }
 
-let LinePositions = {
-    coordinate1_2: undefined,
-    coordinate2_3: undefined,
-    coordinate1_3: undefined
+function Line(PointA, PointB) {
+    this.canvas_x1 = PointA.canvas_x;
+    this.canvas_x2 = PointB.canvas_x;
+    this.canvas_y1 = PointA.canvas_y;
+    this.canvas_y2 = PointB.canvas_y;
+    this.window_x1 = PointA.window_x;
+    this.window_x2 = PointB.window_x;
+    this.window_y1 = PointA.window_y;
+    this.window_y2 = PointB.window_y;
+
+    this.distance = function() {
+        let dist_x = Math.pow(this.canvas_x2 - this.canvas_x1, 2);
+        let dist_y = Math.pow(this.canvas_y2 - this.canvas_y1, 2);
+        return Math.sqrt(dist_x + dist_y);
+    };
 }
 
-let recentPos = undefined;
-let triangleDrawn = false;
+let Points = {
+    point1: undefined,
+    point2: undefined,
+    point3: undefined,
 
-// - - - - - - - - - - - - - - - - - - - - OBJECT MEMORY FUNCTIONS  - - - - - - - - - - - - - - - - - - - - 
-function clearStorage() {
-    for (let key in Positions) {
-        Positions[key] = undefined;
-    }
-
-    for (let key in LinePositions) {
-        LinePositions[key] = undefined;
-    }
-
-    recentPos = undefined;
-}
-
-function addToStorage(coordinates, o) {
-    for (let key in o) {
-        if (typeof o[key] == 'undefined') {
-            o[key] = coordinates;
-            break;
+    clear: function() {
+        for (let key in this) {
+            if (typeof this[key] != 'function') {
+                this[key] = undefined;
+            }
+        }
+    },
+    store: function(point) {
+        for (let key in this) {
+            if (typeof this[key] == 'undefined') {
+                this[key] = point;
+                break;
+            }
         }
     }
 }
-// - - - - - - - - - - - - - - - - - - - -  OBJECT MEMORY FUNCTIONS - - - - - - - - - - - - - - - - - - - - 
 
+let Triangle = {
+    sideA: undefined,
+    sideB: undefined,
+    sideC: undefined,
+
+    area: function () {
+        let s = 0;
+        for (let side in this) {
+            if (typeof this[side] == 'Line') {
+                s += this[side].distance() * 0.5;
+            }
+            let s_a = s-this.sideA.distance();
+            let s_b = s-this.sideB.distance();
+            let s_c = s-this.sideC.distance();
+
+            let a = Math.sqrt(s*s_a*s_b*s_c);
+            return Math.trunc(a);
+        }
+
+    },
+    store: function(line) {
+        for (let key in this) {
+            if (typeof this[key] == 'undefined') {
+                this[key] = line;
+            }
+        }
+    },
+    clear: function() {
+        keys = 0;
+        for (let key in this) {
+            if (typeof this[key] != 'function') {
+                console.log(`Reset ${keys} key[s] from Triangle object`);
+                this[key] = undefined;
+            }
+        }
+    }
+}
+
+let recentPoint = new Point(undefined, undefined);
+let triangleDrawn = false;
+
+function clearStorage() {
+    Triangle.clear();
+    Points.clear();
+    recentPoint = new Point(undefined, undefined);
+    clicks = 1;
+}
 // - - - - - - - - - - - - - - - - - - - -  CANVAS FACING FUNCTIONS - - - - - - - - - - - - - - - - - - - - 
 function clearCanvas() {
     let canvas = document.getElementById('triangle');
@@ -48,19 +108,16 @@ function clearCanvas() {
 function canvasActive(event) {
     if (clicks < 4) { 
         clearCanvas();
-
-        drawAllDots();
+        drawAllPoints();
         drawAllLines(1, "gray");
-
-        showCoordinates(event);
-        showLine(event, recentPos);
+        showPoint(event);
+        showLine(event, recentPoint);
     } else {
         triangleDrawn = true;
     }
 }
 
 function resetCanvas() {
-    clicks = 1;
     triangleDrawn = false;
     clearStorage();
     clearCanvas();
@@ -83,128 +140,99 @@ function canvasInactive() {
 
 // - - - - - - - - - - - - - - - - - - - -  COORDINATE FUNCTIONS - - - - - - - - - - - - - - - - - - - -  
 function setCoordinate(event) {
-    let xy = getPos(event.clientX, event.clientY);
-
+    let pointClicked = new Point(event.clientX, event.clientY);
     if (clicks < 3) {
-        let coordinates = `Coordinates ${clicks}: (${xy[0]}, ${Math.trunc(xy[1])})`;
-        document.getElementById(`coordinates-${clicks}`).innerHTML = coordinates;
-        document.getElementById(`coordinates-${clicks+1}`).innerHTML = `Coordinates ${clicks+1}: `;
+        let point = `Coordinate ${clicks}: (${pointClicked.canvas_x}, ${pointClicked.canvas_y})`;
+        document.getElementById(`coordinates-${clicks}`).innerHTML = point;
+        document.getElementById(`coordinates-${clicks+1}`).innerHTML = `Coordinate ${clicks+1}: `;
     }
 
-    addToStorage(xy, Positions);
-    drawAllDots();
+    Points.store(pointClicked);
+    drawAllPoints();
     clicks++;
 
     if (clicks > 2) {
-        addToStorage([recentPos[0], recentPos[1], xy[0], xy[1]], LinePositions);
+        //addToStorage([recentPoint.x, recentPoint.y, xy[0], xy[1]], Triangle);
+        let line = new Line(recentPoint, pointClicked);
+        Triangle.store(line);
+        console.log(Triangle);
             if (clicks == 4) {
-                addToStorage([Positions.coordinate1[0], Positions.coordinate1[1], xy[0], xy[1]], LinePositions);
+                //addToStorage([Points.point1[0], Points.point1[1], xy[0], xy[1]], Triangle);
+                let line2 = new Line(Points.point1, pointClicked);
+                Triangle.store(line2);
+                console.log(Triangle);
                 drawTriangle("black", "lightblue", 5);
                 document.getElementById('resetButton').innerHTML = "<button class='btn btn-primary' onclick='resetCanvas()'>Reset?</button>";
-                document.getElementById('area').innerHTML = `<h5>${calculateArea(LinePositions)}</h5>`;
+                document.getElementById('area').innerHTML = `<h5>${Triangle.area()}</h5>`;
                 return;
             }
         drawAllLines(1, "gray");
     }
 
-    recentPos = xy;
+    recentPoint = new Point(pointClicked.window_x, pointClicked.window_y);
 }
 
-function getPos(x, y) {
-    let canvas = document.getElementById('triangle');
-    let rect = canvas.getBoundingClientRect();
-    let cx = x - rect.left;
-    let cy = y - rect.top;
-    return [cx, cy];
-}
 
-function calculateDistance(line) {
-    return Math.sqrt(Math.pow(line[1] - line[0], 2) + Math.pow(line[3] - line[2], 2));
-}
-
-function calculateArea(Lines) {
-    let distances = [];
-
-    for (let curr_line in Lines) {
-        distances.push(calculateDistance(Lines[curr_line]));
-    }
-
-    let s = distances.reduce(function(total, num) {
-        return total + num;
-    });
-
-    s /= 2;
-
-    s_a = s-distances[0];
-    s_b = s-distances[1];
-    s_c = s-distances[2];
-
-    let area = Math.sqrt(s*s_a*s_b*s_c);
-    return Math.trunc(area);
-}
 
 // - - - - - - - - - - - - - - - - - - - -  COORDINATE FUNCTIONS - - - - - - - - - - - - - - - - - - - - 
 
 // - - - - - - - - - - - - - - - - - - - -  MOUSEOVER FUNCTIONS - - - - - - - - - - - - - - - - - - - - 
-function showCoordinates(event) {
-    let xy = getPos(event.clientX, event.clientY);
-    let coordinates = `Coordinates ${clicks}: (${xy[0]}, ${Math.trunc(xy[1])})`;
-    document.getElementById(`coordinates-${clicks}`).innerHTML = coordinates;
+function showPoint(event) {
+    let cursor = new Point(event.clientX, event.clientY);
+    let point = `Coordinate ${clicks}: (${cursor.canvas_x}, ${cursor.canvas_y})`;
+    document.getElementById(`coordinates-${clicks}`).innerHTML = point;
 }
 
-function showLine(cursor, xy) {
+function showLine(event, xy) {
+    let cursor = new Point(event.clientX, event.clientY);
     if (clicks == 1 || clicks == 4) {
         return;
     }
-
-    let x = cursor.clientX;
-    let y = cursor.clientY;
-    let cursor_xy = getPos(x, y);
-
-    drawLine(cursor_xy[0], cursor_xy[1], xy[0], xy[1], 1, "gray");
+    let line = new Line(cursor, xy);
+    drawLine(line, 1, "gray");
 
     if (clicks == 3) { //showing line that completes triangle, not setting it yet.
-        drawLine(cursor_xy[0], cursor_xy[1], Positions.coordinate1[0], Positions.coordinate1[1], 1, "gray");
+        let line2 = new Line(cursor, Points.point1);
+        drawLine(line2, 1, "gray");
     }
 }
 // - - - - - - - - - - - - - - - - - - - -  MOUSEOVER FUNCTIONS - - - - - - - - - - - - - - - - - - - - 
 
 // - - - - - - - - - - - - - - - - - - - -  DRAWING FUNCTIONS - - - - - - - - - - - - - - - - - - - - 
-function drawDot(x, y) {
+function drawDot(point) {
    let pointSize = 5;
    let ctx = document.getElementById('triangle').getContext("2d");
    ctx.fillStyle = "#ff2626";
 
    ctx.beginPath();
-   ctx.arc(x, y, pointSize, 0, Math.PI * 2, true);
+   ctx.arc(point.canvas_x, point.canvas_y, pointSize, 0, Math.PI * 2, true);
    ctx.fill();
 
 }
 
-function drawLine(x1, y1, x2, y2, width, style) {
+function drawLine(line, width, style) {
     let canvas = document.getElementById('triangle');
     let ctx = canvas.getContext('2d');
     ctx.lineWidth = width;
     ctx.beginPath();
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2 ,y2);
+    ctx.moveTo(line.canvas_x1, line.canvas_y1);
+    ctx.lineTo(line.canvas_x2, line.canvas_y2);
     ctx.strokeStyle = style;
     ctx.stroke();
 }
 
-function drawAllDots() {
-    for (let key in Positions) {
-        if (typeof Positions[key] != 'undefined') {
-            drawDot(Positions[key][0], Positions[key][1]);
+function drawAllPoints() {
+    for (let key in Points) {
+        if (typeof Points[key] != 'undefined' && typeof Points[key] != 'function') {
+            drawDot(Points[key]);
         }
     }
 }
 
 function drawAllLines(width, style) {
-    for (let key in LinePositions) {
-        if (typeof LinePositions[key] != 'undefined') {
-            let line = LinePositions[key];
-            drawLine(line[0], line[1], line[2], line[3], width, style);
+    for (let key in Triangle) {
+        if (typeof Triangle[key] != 'undefined' && typeof Triangle[key] != 'function') {
+            drawLine(Triangle[key], width, style);
         }
     }
 }
@@ -214,9 +242,9 @@ function drawTriangle(stroke, fill, strokeWidth) {
     let ctx = canvas.getContext('2d');
 
     ctx.beginPath();
-    ctx.moveTo(Positions.coordinate1[0], Positions.coordinate1[1]);
-    ctx.lineTo(Positions.coordinate2[0], Positions.coordinate2[1]);
-    ctx.lineTo(Positions.coordinate3[0], Positions.coordinate3[1]);
+    ctx.moveTo(Points.point1.canvas_x, Points.point1.canvas_y);
+    ctx.lineTo(Points.point2.canvas_x, Points.point2.canvas_y);
+    ctx.lineTo(Points.point3.canvas_x, Points.point3.canvas_y);
     ctx.closePath();
 
     ctx.lineWidth = strokeWidth;
@@ -226,4 +254,5 @@ function drawTriangle(stroke, fill, strokeWidth) {
     ctx.fillStyle = fill;
     ctx.fill();
 }
+
 // - - - - - - - - - - - - - - - - - - - - DRAWING FUNCTIONS - - - - - - - - - - - - - - - - - - - - 
